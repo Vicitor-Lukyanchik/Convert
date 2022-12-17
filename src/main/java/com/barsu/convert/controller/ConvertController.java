cc289003fea1b8c0590ea84c31e290e138377f41
Vicitor <victorlukyanchik15@gmail.com>
2022-12-17 14:47:08 +0300

    create study and events

--- src/main/java/com/barsu/convert/controller/ConvertController.java
+++ src/main/java/com/barsu/convert/controller/ConvertController.java
@@ -22,7 +22,11 @@ import java.nio.file.Paths;
 public class ConvertController {
 
     private final ToPdfConverter toPdfConverter;
+    private final String APPLICATION_DOCX = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
     private final String UPLOAD_DIR = "C:/Users/user/Desktop/PdfFile/convert.pdf";
+    private final String FIRST_STUDY_DIR = "C:/Users/user/Desktop/PdfFile/first.docx";
+    private final String SECOND_STUDY_DIR = "C:/Users/user/Desktop/PdfFile/second.docx";
+    private final String EVENTS_DIR = "C:/Users/user/Desktop/PdfFile/events.docx";
 
     @GetMapping("/index")
     public String sayHello(@ModelAttribute("convert") Convert convert) {
@@ -38,7 +42,53 @@ public class ConvertController {
 
         return ResponseEntity.ok()
                 .contentLength(file.length())
-                .contentType(MediaType.parseMediaType(MediaType.APPLICATION_PDF_VALUE)).body(resource);
+                .contentType(MediaType.parseMediaType(MediaType.APPLICATION_PDF_VALUE))
+                .body(resource);
     }
 
+    @GetMapping(path = "/study")
+    public String getStudy(){
+        return "study";
+    }
+
+    @GetMapping(path = "/study/download/first")
+    public ResponseEntity<Resource> downloadFirstStudy() throws IOException {
+        File file = new File(FIRST_STUDY_DIR);
+        Path path = Paths.get(file.getAbsolutePath());
+        ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+
+        return ResponseEntity.ok()
+                .contentLength(file.length())
+                .contentType(MediaType.parseMediaType(APPLICATION_DOCX))
+                .body(resource);
+    }
+
+    @GetMapping(path = "/study/download/second")
+    public ResponseEntity<Resource> downloadSecondStudy() throws IOException {
+        File file = new File(SECOND_STUDY_DIR);
+        Path path = Paths.get(file.getAbsolutePath());
+        ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+
+        return ResponseEntity.ok()
+                .contentLength(file.length())
+                .contentType(MediaType.parseMediaType(APPLICATION_DOCX))
+                .body(resource);
+    }
+
+    @GetMapping(path = "/events")
+    public String getEvents(){
+        return "events";
+    }
+
+    @GetMapping(path = "/events/download")
+    public ResponseEntity<Resource> downloadEvents() throws IOException {
+        File file = new File(EVENTS_DIR);
+        Path path = Paths.get(file.getAbsolutePath());
+        ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+
+        return ResponseEntity.ok()
+                .contentLength(file.length())
+                .contentType(MediaType.parseMediaType(APPLICATION_DOCX))
+                .body(resource);
+    }
 }
